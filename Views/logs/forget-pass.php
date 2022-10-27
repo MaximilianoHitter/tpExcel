1d7eae8e3cb6f487f33927774390b6a57e4a9252
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2022-10-27 16:19:47 -0300

    Cambios drasticos

--- Views/logs/forget-pass.php
+++ Views/logs/forget-pass.php
@@ -2,9 +2,13 @@
     require_once('../templates/header.php');
 ?>
 
-<div>
-    <h2>No dio pal presupuesto</h2>
-    <h3>Create una cuenta nueva rey</h3>
+<div class="container">
+    <div class="col-md">
+        <div class="row">
+            <img src="../../Public/img/Aqui-no-hacemos-eso.jpg" height="500"  alt="aquinohacemoseso">
+        </div>
+    </div class="container">
+    <h3 class="d-flex justify-content-center m-3">Create una cuenta nueva rey</h3>
 </div>
 
 <?php
